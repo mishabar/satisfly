d7455579b8760a8c453fc2ac6770bfc297974d7d
Michael Bar <barmic@gmail.com>
2014-07-17 19:55:11 -0300

    Fixid last commit

---
+++ EliteRoute.Data/Repositories/IComplaintRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EliteRoute.Data.Model;
+
+namespace EliteRoute.Data.Repositories
+{
+    public interface IComplaintRepository
+    {
+        Complaint Create(Complaint complaint);
+
+        IEnumerable<Complaint> GetAllByEmail(string email);
+
+        Complaint GetByID(string email, long id);
+    }
+}
