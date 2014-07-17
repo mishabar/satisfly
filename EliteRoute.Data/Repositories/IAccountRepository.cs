d877257f0a14d1d4d416455881d6caef7fc6ce27
Michael Bar <barmic@gmail.com>
2014-07-16 22:43:21 -0300

    Fixing build

---
+++ EliteRoute.Data/Repositories/IAccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EliteRoute.Data.Model;
+
+namespace EliteRoute.Data.Repositories
+{
+    public interface IAccountRepository
+    {
+        Account Create(Account account);
+
+        Account FindByEmail(string email);
+    }
+}
---
+++ EliteRoute.Services/IAccountService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EliteRoute.Services.Tokens;
+
+namespace EliteRoute.Services
+{
+    public interface IAccountService
+    {
+        AccountToken Create(string email, string password, string firstName, string lastName, DateTime birthdate, string[] address);
+        AccountToken UpdateEliteStatus(string email, IList<LoyaltyToken> loyalties);
+        AccountToken UpdateBillingInfo(string email, BillingInfoToken billingInfo);
+        AccountToken Get(string email);
+        AccountToken Authenticate(string email, string password);
+    }
+}
